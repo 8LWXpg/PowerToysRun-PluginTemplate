a42462d52bbc992695ad5189c5de2da594e7ad13
8LWXpg <105704427+8LWXpg@users.noreply.github.com>
2025-02-10 18:22:26 +0800

    use lf and tab indent

--- Main.cs
+++ Main.cs
@@ -13,150 +13,150 @@ using Wox.Plugin.Logger;
 
 namespace Community.PowerToys.Run.Plugin.$safeprojectname$
 {
-    public class Main : IPlugin, IPluginI18n, IContextMenu, ISettingProvider, IReloadable, IDisposable, IDelayedExecutionPlugin
-    {
-        private const string Setting = nameof(Setting);
-        // current value of the setting
-        private bool _setting;
-        private PluginInitContext _context;
-        private string _iconPath;
-        private bool _disposed;
-        public string Name => Resources.plugin_name;
-        public string Description => Resources.plugin_description;
-        // TODO: remove dash from ID below and inside plugin.json
-        public static string PluginID => "$guid1$";
+	public class Main : IPlugin, IPluginI18n, IContextMenu, ISettingProvider, IReloadable, IDisposable, IDelayedExecutionPlugin
+	{
+		private const string Setting = nameof(Setting);
+		// current value of the setting
+		private bool _setting;
+		private PluginInitContext _context;
+		private string _iconPath;
+		private bool _disposed;
+		public string Name => Resources.plugin_name;
+		public string Description => Resources.plugin_description;
+		// TODO: remove dash from ID below and inside plugin.json
+		public static string PluginID => "$guid1$";
 
-        // TODO: add additional options (optional)
-        public IEnumerable<PluginAdditionalOption> AdditionalOptions => new List<PluginAdditionalOption>()
-        {
-            new PluginAdditionalOption()
-            {
-                PluginOptionType= PluginAdditionalOption.AdditionalOptionType.Checkbox,
-                Key = Setting,
-                DisplayLabel = Resources.plugin_setting,
-            },
-        };
+		// TODO: add additional options (optional)
+		public IEnumerable<PluginAdditionalOption> AdditionalOptions => new List<PluginAdditionalOption>()
+		{
+			new PluginAdditionalOption()
+			{
+				PluginOptionType= PluginAdditionalOption.AdditionalOptionType.Checkbox,
+				Key = Setting,
+				DisplayLabel = Resources.plugin_setting,
+			},
+		};
 
-        public void UpdateSettings(PowerLauncherPluginSettings settings)
-        {
-            _setting = settings?.AdditionalOptions?.FirstOrDefault(x => x.Key == Setting)?.Value ?? false;
-        }
+		public void UpdateSettings(PowerLauncherPluginSettings settings)
+		{
+			_setting = settings?.AdditionalOptions?.FirstOrDefault(x => x.Key == Setting)?.Value ?? false;
+		}
 
-        // TODO: return context menus for each Result (optional)
-        public List<ContextMenuResult> LoadContextMenus(Result selectedResult)
-        {
-            return new List<ContextMenuResult>(0);
-        }
+		// TODO: return context menus for each Result (optional)
+		public List<ContextMenuResult> LoadContextMenus(Result selectedResult)
+		{
+			return new List<ContextMenuResult>(0);
+		}
 
-        // TODO: return query results
-        public List<Result> Query(Query query)
-        {
-            ArgumentNullException.ThrowIfNull(query);
+		// TODO: return query results
+		public List<Result> Query(Query query)
+		{
+			ArgumentNullException.ThrowIfNull(query);
 
-            var results = new List<Result>();
+			var results = new List<Result>();
 
-            // empty query
-            if (string.IsNullOrEmpty(query.Search))
-            {
-                results.Add(new Result
-                {
-                    Title = Name,
-                    SubTitle = Description,
-                    QueryTextDisplay = string.Empty,
-                    IcoPath = _iconPath,
-                    Action = action =>
-                    {
-                        return true;
-                    },
-                });
-                return results;
-            }
+			// empty query
+			if (string.IsNullOrEmpty(query.Search))
+			{
+				results.Add(new Result
+				{
+					Title = Name,
+					SubTitle = Description,
+					QueryTextDisplay = string.Empty,
+					IcoPath = _iconPath,
+					Action = action =>
+					{
+						return true;
+					},
+				});
+				return results;
+			}
 
-            return results;
-        }
+			return results;
+		}
 
-        // TODO: return delayed query results (optional)
-        public List<Result> Query(Query query, bool delayedExecution)
-        {
-            ArgumentNullException.ThrowIfNull(query);
+		// TODO: return delayed query results (optional)
+		public List<Result> Query(Query query, bool delayedExecution)
+		{
+			ArgumentNullException.ThrowIfNull(query);
 
-            var results = new List<Result>();
+			var results = new List<Result>();
 
-            // empty query
-            if (string.IsNullOrEmpty(query.Search))
-            {
-                return results;
-            }
+			// empty query
+			if (string.IsNullOrEmpty(query.Search))
+			{
+				return results;
+			}
 
-            return results;
-        }
+			return results;
+		}
 
-        public void Init(PluginInitContext context)
-        {
-            _context = context ?? throw new ArgumentNullException(nameof(context));
-            _context.API.ThemeChanged += OnThemeChanged;
-            UpdateIconPath(_context.API.GetCurrentTheme());
-        }
+		public void Init(PluginInitContext context)
+		{
+			_context = context ?? throw new ArgumentNullException(nameof(context));
+			_context.API.ThemeChanged += OnThemeChanged;
+			UpdateIconPath(_context.API.GetCurrentTheme());
+		}
 
-        public string GetTranslatedPluginTitle()
-        {
-            return Resources.plugin_name;
-        }
+		public string GetTranslatedPluginTitle()
+		{
+			return Resources.plugin_name;
+		}
 
-        public string GetTranslatedPluginDescription()
-        {
-            return Resources.plugin_description;
-        }
+		public string GetTranslatedPluginDescription()
+		{
+			return Resources.plugin_description;
+		}
 
-        private void OnThemeChanged(Theme oldTheme, Theme newTheme)
-        {
-            UpdateIconPath(newTheme);
-        }
+		private void OnThemeChanged(Theme oldTheme, Theme newTheme)
+		{
+			UpdateIconPath(newTheme);
+		}
 
-        private void UpdateIconPath(Theme theme)
-        {
-            if (theme == Theme.Light || theme == Theme.HighContrastWhite)
-            {
-                _iconPath = "Images/$projectname$.light.png";
-            }
-            else
-            {
-                _iconPath = "Images/$projectname$.dark.png";
-            }
-        }
+		private void UpdateIconPath(Theme theme)
+		{
+			if (theme == Theme.Light || theme == Theme.HighContrastWhite)
+			{
+				_iconPath = "Images/$projectname$.light.png";
+			}
+			else
+			{
+				_iconPath = "Images/$projectname$.dark.png";
+			}
+		}
 
-        public Control CreateSettingPanel()
-        {
-            throw new NotImplementedException();
-        }
+		public Control CreateSettingPanel()
+		{
+			throw new NotImplementedException();
+		}
 
-        public void ReloadData()
-        {
-            if (_context is null)
-            {
-                return;
-            }
+		public void ReloadData()
+		{
+			if (_context is null)
+			{
+				return;
+			}
 
-            UpdateIconPath(_context.API.GetCurrentTheme());
-        }
+			UpdateIconPath(_context.API.GetCurrentTheme());
+		}
 
-        public void Dispose()
-        {
-            Dispose(true);
-            GC.SuppressFinalize(this);
-        }
+		public void Dispose()
+		{
+			Dispose(true);
+			GC.SuppressFinalize(this);
+		}
 
-        protected virtual void Dispose(bool disposing)
-        {
-            if (!_disposed && disposing)
-            {
-                if (_context != null && _context.API != null)
-                {
-                    _context.API.ThemeChanged -= OnThemeChanged;
-                }
+		protected virtual void Dispose(bool disposing)
+		{
+			if (!_disposed && disposing)
+			{
+				if (_context != null && _context.API != null)
+				{
+					_context.API.ThemeChanged -= OnThemeChanged;
+				}
 
-                _disposed = true;
-            }
-        }
-    }
+				_disposed = true;
+			}
+		}
+	}
 }
--- release.ps1
+++ release.ps1
@@ -12,20 +12,20 @@ git push --tags
 
 Remove-Item ./out/*.zip -Recurse -Force -ErrorAction Ignore
 foreach ($arch in $archs) {
-    $releasePath = "./bin/$arch/Release/net9.0-windows"
+	$releasePath = "./bin/$arch/Release/net9.0-windows"
 
-    dotnet build -c Release /p:Platform=$arch
+	dotnet build -c Release /p:Platform=$arch
 
-    Remove-Item "$tempDir/*" -Recurse -Force -ErrorAction Ignore
-    mkdir "$tempDir" -ErrorAction Ignore
-    $items = @(
-        "$releasePath/$assembly.deps.json",
-        "$releasePath/$assembly.dll",
-        "$releasePath/plugin.json",
-        "$releasePath/Images"
-    )
-    Copy-Item $items "$tempDir" -Recurse -Force
-    Compress-Archive "$tempDir" "./out/$name-$version-$arch.zip" -Force
+	Remove-Item "$tempDir/*" -Recurse -Force -ErrorAction Ignore
+	mkdir "$tempDir" -ErrorAction Ignore
+	$items = @(
+		"$releasePath/$assembly.deps.json",
+		"$releasePath/$assembly.dll",
+		"$releasePath/plugin.json",
+		"$releasePath/Images"
+	)
+	Copy-Item $items "$tempDir" -Recurse -Force
+	Compress-Archive "$tempDir" "./out/$name-$version-$arch.zip" -Force
 }
 
 gh release create $version (Get-ChildItem ./out/*.zip)
